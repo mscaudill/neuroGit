827f7468b901079f2b72c8cb704b0809de795389
MSC <mscaudill@gmail.com>
2015-10-21 10:11:59 -0700

    added cells to be plotted to plot listings

--- data-analysis/ePhys/PlotListings.m
+++ data-analysis/ePhys/PlotListings.m
@@ -5,4 +5,16 @@
 
 % 10-19-2015 Lab meeting talk: Whole-cell pyramidal cells used in analysis
 % for talk.
-% 
+% Figure for w-c inhibition csPeakCurrents/csCharge/csF1Amplitude (binary)
+% 05012015c1 1/1/0 vstim phase misaligned
+% 05042015c1 1/1/0 vstim phase misaligned
+% 05112015c2 1/1/1
+% 05152015c1 1/1/1
+% 05182015c1 1/1/1
+% 05202015c1 1/1/1
+% 05212015c1 1/1/1
+% 05212015c2 1/1/1
+% 05272015c1 1/1/1
+% 06092015c1 1/1/1
+% 07172015c1 1/1/1
+% 07172015c2 1/1/1
